9323577f881ceb03319da4565847d0160bc468d9
Abstrqt <firstkevinshen@gmail.com>
2021-07-09 19:53:41 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ColabTurtleClass',
+    version='0.2.0',
+    packages=find_packages(include=['ColabTurtleClass']),
+    license='MIT',
+    author='Kevin Shen',
+    description='Class Implementation of ColabTurtle',
+    url = 'https://github.com/abstrqt/ColabTurtleClass',
+    install_requires=["IPython"]
+)
